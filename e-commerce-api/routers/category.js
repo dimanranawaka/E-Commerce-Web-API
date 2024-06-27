3adbe96d6b60dad0d0120e3800a1688d7799e8e9
dimanranawaka <dimanranawaka@gmail.com>
2024-06-27 14:21:22 +0530

    add and delete categories

--- e-commerce-api/routers/category.js
+++ e-commerce-api/routers/category.js
@@ -24,4 +24,16 @@ router.post('/',async (req,res)=>{
     res.send(category);
 });
 
+router.delete('/:id',async(req,res)=>{
+    Category.findByIdAndDelete(req.params.id).then(category=>{
+        if(category){
+            return res.status(200).json({success:true,message:'the category is deleted!'});
+        }else{
+            return res.status(404).json({success:false,message:'category not found!'});
+        }
+    }).catch(err=>{
+        return res.status(400).json({success:false,error:err});
+    })
+})
+
 module.exports = router;
