b1f1a2bbced3d57aa51ca61c7d878c1e083c569b
dimanranawaka <dimanranawaka@gmail.com>
2024-06-27 13:04:15 +0530

    product model and scheme

--- e-commerce-api/models/productSchema.js
+++ e-commerce-api/models/productSchema.js
@@ -3,11 +3,59 @@ const { ObjectId } = mongoose.Schema.Types;
 
 const productSchema = new mongoose.Schema({
     // id:Number,
-    name:String,
-    image:String,
+    name:{
+        name: String,
+        required:true
+    },
+    description:{
+      type:String,
+      required:true
+    },
+    richDescription:{
+        type:String,
+        default:''
+    },
+    image:{
+        type:String,
+        default:''
+    },
+    images:[{
+        type:String
+    }],
+    brand:{
+        type:String,
+        default:''
+    },
+    price:{
+        type:Number,
+        default:0
+    },
+    category:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Category', // this will connect to the Category model
+        required:true
+    },
     countInStock:{
         type:Number,
-        required:true
+        required:true,
+        min:0,
+        max:255
+    },
+    rating:{
+        type:Number,
+        default:0
+    },
+    numReviews:{
+        type:Number,
+        default:0
+    },
+    isFeatured:{
+        type:Boolean,
+        default:false
+    },
+    dateCreated:{
+        type:Date,
+        default:Date.now
     }
 })
 
